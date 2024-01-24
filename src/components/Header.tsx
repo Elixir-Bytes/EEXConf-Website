5c1696cb2b0ac98b960a065bce65e77f9c19ce8c
Aaron Cruz <pferdefleisch+git@gmail.com>
2024-01-24 14:14:38 +0100

    Update date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,8 @@ export function Header() {
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-sm text-blue-600 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <p>
-              <time dateTime="2022-04-04">23</time>-
-              <time dateTime="2022-04-06">24 of April, 2024</time>
+              <time dateTime="2024-02-23">23</time>-
+              <time dateTime="2024-02-24">24 of February, 2024</time>
             </p>
             <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
             <p>Zagreb, Croatia</p>
